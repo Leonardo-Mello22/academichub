05f0fe47917661ac091279148589ed72acaf585f
Leonardo-Mello22 <leonardojadm@gmail.com>
2026-09-16 17:01:16 -0300

    Update

--- academic-hub/entrypoint.py
+++ academic-hub/entrypoint.py
@@ -31,4 +31,5 @@ if __name__ == '__main__':
         print("[*] Banco de dados populado com dados iniciais.")
     else:
         print("[*] Banco de dados já populado, pulando seed.")
-    app.run(host='0.0.0.0', port=31337, debug=True)
+    port = int(os.environ.get('PORT', 31337))
+    app.run(host='0.0.0.0', port=port, debug=True)
